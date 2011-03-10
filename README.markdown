4d174ca36a90afb2c099cb4e19033be2750ae841
P. Envall <petter.envall@gmail.com>
2011-03-10 23:39:07 +0100

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,144 @@
+ActsAs
+=========
+
+#### A JavaScript duck typing checker module ####
+
+Mission statement
+-----------------
+When operating on an object in JavaScript, one often wants to check for the object's capabilities instead of its origin.
+This means checking for it having a couple of specified properties, instead of doing something like `obj instanceof Foo` - sometimes there isn't even a `Foo` constructor to check for.
+
+
+Of course this could be taken *really seriously*, leading to writing veritable unit tests for the poor object that wants to quack for us.
+A middle ground is checking for *existence and type* of the properties that make an object able to quack like the duck of your choice.
+
+*This module is meant to make it easy to set up such a quacking standard, and check objects for conformance.*
+
+
+
+Using ActsAs
+------------
+
+Obviously, you need to include the file `ActsAs.js`.
+
+Then, create a `signature` for the characteristics you are after. A signaure is an object with a defined characteristic for each property you are interested in validating.
+
+    // an entity with name and age, is insane and can perform lambada
+    var insaneLambadaEnabled = {
+      name: "type-string"
+      , age: "type-number"
+      , lambada: "type-function"
+      , insane: "value-true"
+    };
+
+Check for conformance with this signature by calling Acts.As(signature, obj):
+
+    function foo(obj) {
+      if (!Acts.As(insaneLambadaEnabled, obj)) return; // failing early. no lambada for you, obj!
+      console.log("I hereby announce our next contestant, an insane lambada dancer named "+obj.name+"!");
+      obj.lambada();
+    }
+    
+    // A dedicated lambadaist constructor
+    function Lambadaist(name, age, insane) {
+      this.name = name;
+      this.age = age;
+      this.insane = !!insane;
+    }
+    Lambadaist.prototype.lambada = function () {
+      return console.log(this.name+": NAAA NA-NA-NA-NAAA...");
+    };
+    
+    // Middle-aged and insane object named Ljörgen has lambada chops
+    foo({name:"Ljörgen", age: 47, insane: true, lambada: function () {console.log(this.name+": Naaa na-na-na-naaa... (ducky)");}});
+    // A card carrying Lambdaist obj has lambada chops..
+	var lambadator = new Lambadaist("Kalenderhielm", 56, true);
+    foo(lambadator);  // ok
+	// .. after being on medication for a while, no longer applicable for foo()
+	lambadator.insane = false;
+	foo(lambadator); // throws
+
+    
+
+
+The result is `true`, or `false`. In the case of `false`, you might be interested in what went wrong. Failure info (first point of failure), is available via `Acts.As.info()`.
+
+Here's how to use conformance failure info for debugging or whatever:
+
+    function bar(obj) {
+      if (!Acts.As(insaneLambadaEnabled, obj)) {throw Error(Acts.As.info());}
+      obj.lambada();
+    }
+
+    // will throw with msg "prop 'insane' was not value-true, but: string (TRUE)"
+    bar({name:"Ljörgen", age: 47, insane: "TRUE", lambada: function () {console.log(this.name+": Naaa na-na-na-naaa... (ducky)");}});
+
+
+Characteristics
+---------------
+
+Out of the box, a signature explicitly supports the following property characteristics:
+	
+*Types*
+
+   	"type-string"
+    "type-number"
+    "type-function"
+    "type-regexp"
+    "type-array"
+	"type-date"
+	"type-boolean"      // any boolean will do
+
+*Values*
+
+	"value-true"        // property must be === true
+    "value-false"       // property must be === false
+    "value-null"        // property must be === null
+    "value-undefined"   // property must not be defined
+
+*Constrained values*
+
+	"positive-number"
+    "negative-number"
+	"integer"
+	"positive-integer"
+	"negative-integer"
+	"non-blank-string"
+	
+*Constructors*
+
+	Foo		// property must be instanceof Foo
+	
+The constructor validating thing is demonstrated in the `logGrunt.signature` below.
+We also see how one could go about validating function parameters in general:
+	
+	function logGrunt(ape, what) {
+	  if (!Acts.As(logGrunt.signature, {"gruntee": ape, "what": what})) {throw Error(Acts.As.info());}
+	  console.log(ape.grunt(what));
+	}
+	logGrunt.signature = {
+	  "gruntee": Ape
+	  , "what": "non-blank-string"
+	};
+	function Ape(name) {
+	  this.name = name;
+	}
+	Ape.prototype.grunt = function (what) {return this.name+ " grunts: '"+what+"'";};
+	
+
+	logGrunt(new Ape("Ljörgen"), "fool.."); // ok
+	logGrunt("Ljörgen", "fool.."); // throws
+
+
+
+Custom validations are coming up when I decided on a nice API for it...
+
+
+Tip
+---
+
+As shown above, a nice place for signatures can be on a function that is meant to operate on just a certain breed of objects.
+
+
+Happy ducktyping!  
+/me
---
+++ tests/qUnit/tests.js
@@ -0,0 +1,186 @@
+(function () {
+
+// Example model
+function Mud(server, port) {
+  this.server = server;
+  this.port = port;
+}
+
+var TestRunner = (function() {
+var t0 = new Date(2003, 1, 28)
+, t1 = new Date(2043, 1, 28)
+ , mud = new Mud("grimne.org", 4000)
+, tests = {
+  "string (foo)":
+			{value:"foo",       tags: {"type-string": true, "non-blank-string": true}}
+			
+  , "string ()":
+			{value:"",          tags: {"type-string": true}}
+			
+  , "number (42)":
+			{value:42,          tags: {"type-number": true, "integer": true, "positive-number": true, "positive-integer": true}}
+			
+  , "number (-42)":
+			{value:-42,         tags: {"type-number": true, "integer": true, "negative-number": true, "negative-integer": true}}
+			
+  , "number (42.5)":
+			{value:42.5,        tags: {"type-number": true, "positive-number": true}}
+			
+  , "number (-42.5)":
+			{value:-42.5,       tags: {"type-number": true, "negative-number": true}}
+			
+  , "boolean (true)":
+			{value:true,        tags: {"type-boolean": true, "value-true": true}}
+			
+  , "boolean (false)":
+			{value:false,       tags: {"type-boolean": true, "value-false": true}}
+			
+  , "array (1,2,3)":
+			{value:[1,2,3],     tags: {"type-array": true}}
+			
+  , "function":
+			{value:function(){},tags: {"type-function": true}}
+			
+  , "regexp":           
+			{value: /foo/,      tags: {"type-regexp": true}, trunc: true}
+			
+  , "undefined":        
+			{value: undefined,  tags: {"value-undefined": true}}
+			
+  , "null":
+			{value: null,       tags: {"value-null": true}}
+};
+
+tests["date (ts:"+(+t0)+")"] =
+			{value: t0,         tags: {"type-date": true, "date-past": true}, trunc: true};
+tests["date (ts:"+(+t1)+")"] =
+			{value: t1,         tags: {"type-date": true, "date-future": true}, trunc: true};
+			
+tests["instance of Mud"] =
+			{value: mud, 	tags: {"instance-of-Mud": true}};
+
+var _opts = Object.prototype.toString;
+function fn(f) {var re = /function (.+)\(/, m = re.exec(f.toString()); return m ? m[1] : "[anonymous function]";}
+function loop(obj, process) {for (var prop in obj) {process(obj, prop);}}
+function run(conf) {
+  var propName = "p"
+    , instanceCheck = (typeof conf[propName]=="function") ? ("instance of " + fn(conf[propName])) : "";
+  loop(tests, function (tests, testName) {
+    var data = tests[testName], testObj = {}, val;
+    testObj[propName] = val = data.value;
+    var result = Acts.As(conf, testObj)
+      , expectedMsg, actualMsg;
+		if (data.tags[conf[propName]] || data.tags[conf.tag]) {ok(result, "OK - "+testName);}
+    else {
+			expectedMsg = "prop '"+propName+"' was not "+(instanceCheck?instanceCheck:conf[propName]);
+      expectedMsg += (", but: "+ testName);
+      actualMsg = Acts.As.info();
+      if (data.trunc) {actualMsg = actualMsg.substring(0, expectedMsg.length);}
+      equal(actualMsg, expectedMsg, "With "+testName);
+    }
+  });
+}
+return {run: run};
+})();
+
+module("Type");
+/*****************************************************
+* Checks on the various types
+******************************************************/
+test("String", function () {
+  TestRunner.run({p: "type-string"});
+});
+
+test("Number", function () {
+  TestRunner.run({p: "type-number"});
+});
+
+test("Boolean", function () {
+  TestRunner.run({p: "type-boolean"});
+});
+
+test("Array", function () {
+  TestRunner.run({p: "type-array"});
+});
+
+test("Function", function () {
+  TestRunner.run({p: "type-function"});
+});
+
+test("RegExp", function () {
+  TestRunner.run({p: "type-regexp"});
+});
+
+test("Date", function () {
+  TestRunner.run({p: "type-date"});
+});
+
+
+module("Value");
+/*****************************************************
+* Checks on values
+******************************************************/
+test("undefined", function () {
+  TestRunner.run({p: "value-undefined"});
+});
+
+test("null", function () {
+  TestRunner.run({p: "value-null"});
+});
+
+test("true", function () {
+  TestRunner.run({p: "value-true"});
+});
+
+test("false", function () {
+  TestRunner.run({p: "value-false"});
+});
+
+
+module("Constrained value");
+/*****************************************************
+* Special checks on values
+******************************************************/
+test("non-blank string", function () {
+  TestRunner.run({p: "non-blank-string"});
+});
+
+test("positive number", function () {
+  TestRunner.run({p: "positive-number"});
+});
+
+test("negative number", function () {
+  TestRunner.run({p: "negative-number"});
+});
+
+test("integer", function () {
+  TestRunner.run({p: "integer"});
+});
+
+test("positive integer", function () {
+  TestRunner.run({p: "positive-integer"});
+});
+
+test("negative integer", function () {
+  TestRunner.run({p: "negative-integer"});
+});
+
+test("past date", function () {
+  TestRunner.run({p: "date-past"});
+});
+
+test("future date", function () {
+  TestRunner.run({p: "date-future"});
+});
+
+
+module("Instance check");
+/*****************************************************
+* Instance check
+******************************************************/
+test("instance of Mud", function () {
+  TestRunner.run({p: Mud, tag:"instance-of-Mud"});
+});
+
+
+})();
